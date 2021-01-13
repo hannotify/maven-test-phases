c0513346d70c71b9ac50a1be05ea264a018b9530
Hanno Embregts <hanno.embregts@infosupport.com>
2021-01-13 07:42:23 +0100

    Initial commit

---
+++ src/main/java/com/github/hannotify/maventestphases/MavenTestPhasesApplication.java
@@ -0,0 +1,13 @@
+package com.github.hannotify.maventestphases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestPhasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTestPhasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hannotify/maventestphases/MavenTestPhasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hannotify.maventestphases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestPhasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
